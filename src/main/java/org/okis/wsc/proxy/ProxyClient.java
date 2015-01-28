87ccca7c1aec504594ab4b0a3487c8916a88e91c
Jan Steinhof <jan.steinhof@objekt-kontor.de>
2015-01-28 15:45:17 +0100

    Added a few lines of documentation

--- src/main/java/org/okis/wsc/proxy/ProxyClient.java
+++ src/main/java/org/okis/wsc/proxy/ProxyClient.java
@@ -17,6 +17,11 @@ import io.netty.util.AttributeKey;
 
 import org.okis.wsc.common.config.ClientConfig;
 
+/**
+ * This class is designed to be the last Handler in the incoming pipeline as well as the last Handler 
+ * of the outgoing one. As such it is responsible for taking over messages from one pipeline to the 
+ * other - as implemented in the channelRead-method. 
+ */
 @Sharable
 public class ProxyClient extends ChannelInboundHandlerAdapter {
 
@@ -61,18 +66,29 @@ public class ProxyClient extends ChannelInboundHandlerAdapter {
         sourceChannel.close();
     }
 
+    /**
+     * This method is called whenever a message reaches this Handler via one of the two pipelines this Handler is a 
+     * member of.
+     */
     @Override
     final public void channelRead(final ChannelHandlerContext context, final Object message) throws Exception {
+        // Three different cases need to be handled:
+        
+        // Case 2: the message comes from the outgoing pipeline (so it is a response) - further handling is done in the receive-method:
         Channel sourceChannel = context.channel().attr(SOURCE_CHANNEL).get();
         if (sourceChannel != null) {
             receive(sourceChannel, message);
             return;
         }
+        
+        // Case 3: the message comes from the incoming pipeline, however there is already a target-channel present - so this one will be used again:
         Channel targetChannel = context.channel().attr(TARGET_CHANNEL).get();
         if (targetChannel != null) {
             send(context, targetChannel, message);
             return;
         }
+
+        // Case 1: the message comes from the incoming pipeline, no connection to the target has yet been established:
         bootstrap.connect(config.getHost(), config.getPort()).addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) throws Exception {
