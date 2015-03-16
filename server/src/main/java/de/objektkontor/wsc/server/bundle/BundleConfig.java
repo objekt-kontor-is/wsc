7ce94e1171295c94b0808f61fa5b22cf3841038e
Eugen Borth <borth@objekt-kontor.de>
2015-03-16 11:09:04 +0100

    add id to bundle config

--- server/src/main/java/de/objektkontor/wsc/server/bundle/BundleConfig.java
+++ server/src/main/java/de/objektkontor/wsc/server/bundle/BundleConfig.java
@@ -1,14 +1,26 @@
 package de.objektkontor.wsc.server.bundle;
 
+import de.objektkontor.config.annotation.ConfigIdentifier;
 import de.objektkontor.config.annotation.ConfigParameter;
 
 public class BundleConfig {
+
+    @ConfigIdentifier
+    private String id;
 
     @ConfigParameter
     private String location;
 
     @ConfigParameter
     private String dependenciesDir;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getLocation() {
         return location;
